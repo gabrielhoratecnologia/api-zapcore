99b1474fc5862521028621d79b24a60721ce6c40
Gabriel Hora <gabrielhoradeveloper@gmail.com>
2026-02-06 20:27:28 -0300

    error: console help

--- src/server.js
+++ src/server.js
@@ -10,15 +10,13 @@ const app = express();
 
 app.use(express.json());
 
-//app.use("/conversations", conversationsRoutes);
+app.use("/conversations", conversationsRoutes);
 
-//app.use("/messages", messagesRoutes);
+app.use("/messages", messagesRoutes);
 
-//app.use("/auth", authRoutes);
+app.use("/auth", authRoutes);
 
-//app.use("/webhooks", webhooksRoutes);
-
-app.get("/health", (req, res) => res.send("OK"));
+app.use("/webhooks", webhooksRoutes);
 
 app.listen(env.port, () => {
   console.log(`🚀 API rodando na porta ${env.port}`);
